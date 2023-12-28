71676e577a37554ddd1040ebecc70d636037a365
Kronos_Codeur <piraliarris@gmail.com>
2023-12-28 18:24:17 +0000

    start csharp learning with variables

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CourseOne;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int note = 18;
+        int moyenne = 0;
+        moyenne = (note + 8) / 2;
+        Console.WriteLine(moyenne);
+        
+    }
+}
+
