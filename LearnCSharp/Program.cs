3bf4d5a171c9b8c54609739cbb72ac6af2f88df8
Kronos_Codeur <piraliarris@gmail.com>
2023-12-28 19:05:13 +0000

    begin tables manipulations learning

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,42 +6,7 @@ class Program
 {
     static void Main()
     {
-        greeting("evening");
-        display(findArea(9,4));
 
-        // function without return
-
-        void greeting(string moment)
-        {
-            switch (moment)
-            {
-                case "morning":
-                    Console.WriteLine("Good " + moment + " everyone, do you sleep well?");
-                    break;
-                case "afternoon":
-                    Console.WriteLine("Good " + moment + " everyone, do you eat anything?");
-                    break;
-                case "evening":
-                    Console.WriteLine("Good " + moment + " everyone, have a good night");
-                    break;
-                default:
-                    Console.WriteLine("unknown moment");
-                    break;
-            }
-        }
-
-        void display(dynamic variable)
-        {
-            Console.WriteLine(variable);
-        }
-
-        //function with return
-
-
-        int findArea(int longueur, int largeur)
-        {
-            return longueur * largeur;
-        }
 
     }
 }
