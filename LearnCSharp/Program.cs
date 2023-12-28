622889d7272cdefb5a4208177165e7d3ff1985d8
Kronos_Codeur <piraliarris@gmail.com>
2023-12-28 19:14:17 +0000

    begin loop learning

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,20 +6,7 @@ class Program
 {
     static void Main()
     {
-        //tables
-        int[] winnersIDs = new int[] { 2, 5, 8, 9, 7, 2 };
-        Console.WriteLine(winnersIDs[0]);
-        winnersIDs[0] = 56;
-        Console.WriteLine(winnersIDs[0]);
 
-        // lists
-        List<string> greetings = new List<string>();
-        greetings.Add("good morning");
-        greetings.Add("good evening");
-        greetings.Reverse();
-        Console.WriteLine(greetings[0]);
-        greetings.Remove(greetings[0]);
-        Console.WriteLine(greetings[0]);
     }
 }
 
