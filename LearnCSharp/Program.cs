d253c87e6a7daa58f8f374057f1cade49f211a3a
Kronos_Codeur <piraliarris@gmail.com>
2023-12-28 21:03:48 +0000

    end Dictionaries(Maps) learning

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,7 +6,41 @@ class Program
 {
     static void Main()
     {
+        Dictionary<int, string> numbers = new Dictionary<int, string>()
+        {
+            {1,"one"},
+            {2,"two"},
+            {3,"three"},
+            {4,"four"},
+            {5,"five"},
+        };
+        Console.WriteLine(numbers[2]);
+        displayADictionnary(numbers);
+        numbers.Remove(5);
+        displayADictionnary(numbers);
+        numbers.Add(6,"six");
+        displayADictionnary(numbers);
+        void displayADictionnary(Dictionary<int, string> dictionary)
+        {
+            foreach (KeyValuePair<int,string> pair in dictionary)
+            {
+                Console.WriteLine(pair.Key + " => " + pair.Value);
+            }
+            Console.WriteLine("\n");
+        }
 
+        Dictionary<string, string> greetingsByMoment = new Dictionary<string, string>()
+        {
+            {
+                "Morning","Good Morning"
+            },
+            {
+                "Afternoon","Afternoon"
+            },
+            {
+                "Evening","Good Evening"
+            }
+        };
     }
 }
 
