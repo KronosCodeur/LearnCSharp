eec2c3d2c7a13905b65418ce705dd90ca186254f
Kronos_Codeur <piraliarris@gmail.com>
2023-12-28 22:43:43 +0000

    end learning csharp basic

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,16 +6,8 @@ class Program
 {
     static void Main()
     {
-        Dog dog = new Dog("Rufus");
-        dog.Eat();
-        dog.Bark();
-        dog.SetIsHungry(true);
-        dog.Eat();
-        Cat cat = new Cat("Santrinos");
-        cat.Eat();
-        cat.Meow();
-        cat.SetIsHungry(true);
-        cat.Eat();
+
+
     }
 
 }
